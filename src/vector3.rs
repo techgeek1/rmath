b5925b6cadedb59e6a79c5d92cbeec21a2fec0f6
austin.rife <austin.rife@gmail.com>
2018-03-30 21:14:51 -0400

    Broken things

--- src/vector3.rs
+++ src/vector3.rs
@@ -1,5 +1,5 @@
 use std::ops::*;
-use math::*;
+use math;
 
 #[derive(Copy, Clone, PartialEq)]
 struct Vector3 {
@@ -10,8 +10,6 @@ struct Vector3 {
 
 #[allow(dead_code)]
 impl Vector3 {
-    const epsilon: f32 = 0.00001;
-
     pub static zero: &'static Vector3 = Vector3{ x: 0.0, y: 0.0, z: 0.0 };
     pub static one: &'static Vector3 = Vector3 { x: 1.0, y: 1.0, z: 1.0 };
     pub static forward &'static Vector3 = Vector3 { x: 0.0, y: 0.0, z: 1.0 };
@@ -25,12 +23,6 @@ impl Vector3 {
             z: z
         }
     }
-    
-    pub fn clamp_magnitude(&self, max_length: f32) -> Vector3 {
-        if self.sqr_magnitude() > max_length * max_length {
-            self.normalized() * max_length;
-        }
-    }
 
     pub fn sqr_magnitude(&self) -> f32 {
         self.x * self.x + self.y * self.y + self.z * self.z
@@ -42,21 +34,21 @@ impl Vector3 {
 
     pub fn normalize(&mut self) {
         let mag = self.magnitude();
-        if mag > epsilon {
+        if mag > f32::EPSILON {
             self = self / mag;
         }
         else {
-            self = zero;
+            self = Vector3::zero;
         }
     }
 
     pub fn normalized(&self) -> Vector3 {
         let mag = self.magnitude();
-        if mag > epsilon {
-            v / mag;
+        if mag > f32::EPSILON {
+            self / mag
         }
         else {
-            zero;
+            Vector3::zero
         }
     }
 }
@@ -80,9 +72,8 @@ impl Vector3 {
     }
 
     pub fn angle(from: Vector3, to: Vector3) -> f32 {
-        clamp(dot(from.normalized(), to.normalized()), -1.0, 1.0)
+        math::clamp(Vector3::dot(from.normalized(), to.normalized()), -1.0, 1.0)
         .acos()
-        .to_degrees()
     }
 
     pub fn scale(a: Vector3, b: Vector3) -> Vector3 {
@@ -104,15 +95,15 @@ impl Vector3 {
     pub fn ortho_normalize(a: &mut Vector3, b: &mut Vector3) -> Vector3 {
         a.normalize();
 
-        let c = cross(a, b);
+        let c = Vector3::cross(a, b);
         c.normalize();
 
-        b = cross(a, c);
+        b = Vector3::cross(a, c);
         b.normalize();
     }
 
     pub fn lerp(a: Vector3, b: Vector3, f32 t) -> Vector3 {
-        let alpha = clamp01(t);
+        let alpha = math::clamp01(t);
 
         Vector3 {
             x: a.x + (b.x - a.x) * alpha,
@@ -129,9 +120,9 @@ impl Vector3 {
         }
     }
 
-    pub fn slerp(a: Vector3, b: Vector3, t: f32) -> Vector3 {
-        let alpha = clamp01(t);
-        let dot = clamp(dot(a, b) -1.0, 1.0);
+    pub fn slerp(start: Vector3, end: Vector3, t: f32) -> Vector3 {
+        let alpha = math::clamp01(t);
+        let dot = math::clamp(Vector3::dot(start, end) -1.0, 1.0);
         let theta = dot.acos() * alpha;
         
         let mut relative = end - start * dot;
@@ -140,8 +131,8 @@ impl Vector3 {
         (start * theta.cos()) + (relative * theta.sin())
     }
 
-    pub fn slerp_unclamped(a: Vector3, b: Vector3, t: f32) -> Vector3 {
-        let dot = clamp(dot(a, b) -1.0, 1.0);
+    pub fn slerp_unclamped(start: Vector3, end: Vector3, t: f32) -> Vector3 {
+        let dot = math::clamp(Vector3::dot(start, end) -1.0, 1.0);
         let theta = dot.acos() * t;
         
         let mut relative = end - start * dot;
@@ -151,31 +142,32 @@ impl Vector3 {
     }
 
     pub fn project(vector: Vector3, normal: Vector3) -> Vector3 {
-        let dot = dot(normal, normal);
+        let dot = Vector3::dot(normal, normal);
         if dot < f32::EPSILON {
-            zero;
+            Vector3::zero
         }
         else {
-            vector * dot(vector, normal) / dot;
+            vector * Vector3::dot(vector, normal) / dot
         }
     }
     
     pub fn project_on_segment(point: Vector3, start: Vector3, end: Vector3) -> Vector3 {
         let segment = end - start;
-        let proj_point = point.project(segment.normalized());
+        let proj_point = Vector3::project(point, segment.normalized());
         
-        clamp_magnitude(proj_point - start, segment.magnitude());
+        Vector3::clamp_magnitude(proj_point - start, segment.magnitude())
     }
 
     pub fn project_on_plane(vector: Vector3, normal: Vector3) -> Vector3 {
-        vector - project(vector, normal);
+        vector - Vector3::project(vector, normal)
     }
 
     pub fn reflect(v: Vector3, normal: Vector3) -> Vector3 {
-        -2.0 * dot(normal, v) * normal + v;
+        -2.0 * Vector3::dot(normal, v) * normal + v
     }
 }
 
+/*
 // Fluent API
 impl Vector3 {
     pub fn dot(&self, other: Vector3) -> f32 {
@@ -197,7 +189,6 @@ impl Vector3 {
     pub fn angle(&self, other: Vector3) -> f32 {
         clamp(dot(self.normalized(), other.normalized()), -1.0, 1.0)
         .acos()
-        .to_degrees()
     }
 
     pub fn scale(&self, other: Vector3) -> Vector3 {
@@ -241,6 +232,7 @@ impl Vector3 {
         -2.0 * dot(normal, self) * normal + self;
     }
 }
+*/
 
 impl Add for Vector3 {
     type Output = Vector3;
@@ -321,10 +313,10 @@ impl Div<f32> for Vector3 {
 }
 
 impl DivAssign<f32> for Vector3 {
-    fn mul_assign(&mut self, other: f32) {
-        self.x /= other;
-        self.y /= other;
-        self.z /= other;
+    fn div_assign(&mut self, other: f32) {
+        self.x = self.x / other;
+        self.y = self.y / other;
+        self.z = self.z / other;
     }
 }
 
@@ -350,46 +342,13 @@ impl ToString for Vector3 {
 mod tests {
     use super::*;
 
-    // Vector methods
-    #[test]
-    fn zero() {
-        let v = Vector3::zero();
-        assert_eq!(v.x, 0.0);
-        assert_eq!(v.y, 0.0);
-        assert_eq!(v.z, 0.0);
-    }
-
+    // Methods
     #[test]
     fn constructor() {
         let v = Vector3::new(1.0, 1.0, 1.0);
         assert_eq!(v.x, 1.0);
         assert_eq!(v.y, 1.0);
         assert_eq!(v.z, 1.0);
-    }
-
-    #[test]
-    fn dot_product() {
-        let left = Vector3::new(-1.0, 0.0, 0.0);
-        let right = Vector3::new(1.0, 0.0, 0.0);
-        let forward = Vector3::new(0.0, 0.0, 1.0);
-
-        let dot_one = Vector3::dot(right, right);
-        assert_eq!(dot_one, 1.0);
-
-        let dot_neg_one = Vector3::dot(right, left);
-        assert_eq!(dot_neg_one, -1.0);
-
-        let dot_zero = Vector3::dot(right, forward);
-        assert_eq!(dot_zero, 0.0);
-    }
-
-    #[test]
-    fn cross_product() {
-        let right = Vector3::new(1.0, 0.0, 0.0);
-        let forward = Vector3::new(0.0, 0.0, 1.0);
-
-        let up = Vector3::cross(forward, right);
-        assert_eq!(up.y, 1.0);
     }
 
     #[test]
@@ -413,10 +372,176 @@ mod tests {
     }
 
     #[test]
-    fn normalize() {
+    fn normalized() {
         let v = Vector3::new(5.0, 0.0, 0.0).normalized();
 
         assert_eq!(v.x, 1.0);
+    }
+    
+    #[test]
+    fn dot_product() {
+        let left = Vector3::new(-1.0, 0.0, 0.0);
+        let right = Vector3::new(1.0, 0.0, 0.0);
+        let forward = Vector3::new(0.0, 0.0, 1.0);
+
+        let dot_one = Vector3::dot(right, right);
+        assert_eq!(dot_one, 1.0);
+
+        let dot_neg_one = Vector3::dot(right, left);
+        assert_eq!(dot_neg_one, -1.0);
+
+        let dot_zero = Vector3::dot(right, forward);
+        assert_eq!(dot_zero, 0.0);
+    }
+
+    #[test]
+    fn dot_product_fluent() {
+        let left = Vector3::new(-1.0, 0.0, 0.0);
+        let right = Vector3::new(1.0, 0.0, 0.0);
+        let forward = Vector3::new(0.0, 0.0, 1.0);
+
+        let dot_one = right.dot(right);
+        assert_eq!(dot_one, 1.0);
+
+        let dot_neg_one = right.dot(left);
+        assert_eq!(dot_neg_one, -1.0);
+
+        let dot_zero = right.dot(forward);
+        assert_eq!(dot_zero, 0.0);
+    }
+    
+    #[test]
+    fn cross_product() {
+        let right = Vector3::new(1.0, 0.0, 0.0);
+        let forward = Vector3::new(0.0, 0.0, 1.0);
+
+        let up = Vector3::cross(forward, right);
+        assert_eq!(up.y, 1.0);
+    }
+    
+    #[test]
+    fn cross_product_fluent() {
+        let right = Vector3::new(1.0, 0.0, 0.0);
+        let forward = Vector3::new(0.0, 0.0, 1.0);
+
+        let up = forward.cross(right);
+        assert_eq!(up.y, 1.0);
+    }
+    
+    #[test]
+    fn distance() {
+        let v0 = Vector3::new(1.0, 0.0, 0.0);
+        let v1 = Vector3::zero;
+        
+        let distance = Vector3::distance(v0, v1);
+        
+        assert_eq!(distance, 1.0);
+    }
+    
+    #[test]
+    fn distance_fluent() {
+        let v0 = Vector3::new(1.0, 0.0, 0.0);
+        let v1 = Vector3::zero;
+        
+        let distance = v0.distance(v1);
+        
+        assert_eq!(distance, 1.0);
+    }
+    
+    #[test]
+    fn angle() {
+        let v0 = Vector3::new(1.0, 0.0, 0.0);
+        let v1 = Vector3::new(0.0, 1.0, 0.0);
+        
+        let angle = Vector3::angle(v0, v1);
+        
+        assert_eq!(angle, 90.0.to_radians());
+    }
+    
+    #[test]
+    fn angle_fluent() {
+        let v0 = Vector3::new(1.0, 0.0, 0.0);
+        let v1 = Vector3::new(0.0, 1.0, 0.0);
+        
+        let angle = v0.angle(v1);
+        
+        assert_eq!(angle, 90.0.to_radians());
+    }
+    
+    #[test]
+    fn scale() {
+        let v = Vector3::one;
+        let v_scaled = Vector3::scale(v, Vector3::one * 4.0);
+        
+        assert_eq!(v_scaled.x, 4.0);
+        assert_eq!(v_scaled.y, 4.0);
+        assert_eq!(v_scaled.z, 4.0);
+    }
+    
+    #[test]
+    fn scale_fluent() {
+        let v = Vector3::one;
+        let v_scaled = v.scale(Vector3::one * 4.0);
+        
+        assert_eq!(v_scaled.x, 4.0);
+        assert_eq!(v_scaled.y, 4.0);
+        assert_eq!(v_scaled.z, 4.0);
+    }
+    
+    #[test]
+    fn clamp_magnitude() {
+        let v = Vector3::one * 10.0;
+        let v_clamped = Vector3::clamp_magnitude(v, 2.0);
+        
+        assert_eq!(v_clamped.magnitude(), 2.0);
+    }
+    
+    #[test]
+    fn clamp_magnitude_fluent() {
+        let v = Vector3::one * 10.0;
+        let v_clamped = v.clamp_magnitude(2.0);
+        
+        assert_eq!(v_clamped.magnitude(), 2.0);   
+    }
+    
+    #[test]
+    fn project() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn project_fluent() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn project_on_segment() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn project_on_segment_fluent() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn project_on_plane() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn project_on_plane_fluent() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn reflect() {
+        assert!(false);
+    }
+    
+    #[test]
+    fn reflect_fluent() {
+        assert!(false);
     }
 
     // Operators
